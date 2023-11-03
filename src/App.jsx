f365a5c53e6057cbb01aa6b22426371abdac47fe
weerotheme <aktech.akash@gmail.com>
2023-11-04 02:50:16 +0600

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-blue-100 h-[100vh]  flex items-center">
+        <div className="max-w-screen-xl w-full p-5 bg-white mx-auto rounded-md border  h-[97%]">
+          <div className="headerTitel border-b py-2">
+            <h1 className="titel text-lg">Gallery</h1>
+
+            <div className="flex justify-between">
+              <label>
+                <input checked type="checkbox" />
+                <span className="select-file-count ml-2 text-base">2</span>{" "}
+                Files Selected
+              </label>
+              <p className="text-[red] text-base">Delete</p>
+            </div>
+          </div>
+          <div className="DragAndDrop-Container mt-4 flex flex-wrap gap-5">
+            <div className="feature-img  w-[400px]  flex-grow-3 rounded-md border">
+              <div className="item-img">
+                <img
+                  className="rounded-md"
+                  src="./assets/image-1.webp"
+                  alt="image-1"
+                />
+              </div>
+            </div>
+
+            <div className="item-img  w-[200px] flex-grow-1">
+              <img
+                className="rounded-md  border "
+                src="./assets/image-2.webp"
+                alt="image-1"
+              />
+            </div>
+            <div className="item-img  w-[200px] flex-grow-1">
+              <img
+                className="rounded-md  border "
+                src="./assets/image-2.webp"
+                alt="image-1"
+              />
+            </div>
+            <div className="item-img  w-[200px] flex-grow-1">
+              <img
+                className="rounded-md  border "
+                src="./assets/image-2.webp"
+                alt="image-1"
+              />
+            </div>
+            <div className="item-img  w-[200px] flex-grow-1">
+              <img
+                className="rounded-md  border "
+                src="./assets/image-2.webp"
+                alt="image-1"
+              />
+            </div>
+            <div className="item-img w-[200px]  flex-grow-1">
+              <img
+                className="rounded-md  border "
+                src="./assets/image-2.webp"
+                alt="image-1"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
